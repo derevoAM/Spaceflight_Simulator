6c9af1247db663e924b84c8c35ff33f40ae28eb4
Ivan Khripunov <khripunov.iv@phystech.edu>
2022-05-03 23:29:32 +0300

    trajectory_calculation.py refactoring improved

--- trajectory_calculation.py
+++ trajectory_calculation.py
@@ -20,7 +20,7 @@ class Constants:
         self.moon_rad = 3.85e8  # радиус орбиты Луны
         self.moon_period = 27.3 * 24 * 3600 / 2 / np.pi  # период обращения Луны вокруг Земли в с/рад
         self.initial_fas = 99 / 180 * np.pi  # начальная фаза Луны (которую нужно подогнать)
-        self.log_size = 300
+        self.log_size = 200
 
         self.gas_exhaust_speed = gas_exhaust_speed  # скорость истечения топлива
         self.fuel_consumption = fuel_consumption  # расход топлива
@@ -31,8 +31,8 @@ class Constants:
 
 class RocketParameters:
 
-    def __init__(self, initial_parameters, initial_rocket_mass, fuel):
-        self.parameters = np.array(initial_parameters)
+    def __init__(self, initial_rocket_mass, fuel):
+        self.parameters = np.array([7000e3, 0, 0, 7559.3])
         self.position_norm = np.linalg.norm(self.parameters[:2])
         self.velocity_norm = np.linalg.norm(self.parameters[2:])
         self.direction = np.array([1, 0])
@@ -53,10 +53,9 @@ class RocketParameters:
 
 class PhysicsEngine:
 
-    def __init__(self, initial_rocket_mass, gas_exhaust_speed, fuel_consumption, fuel_tank_capacity, fuel,
-                 initial_parameters):
+    def __init__(self, initial_rocket_mass, gas_exhaust_speed, fuel_consumption, fuel_tank_capacity, fuel):
         self.constants = Constants(gas_exhaust_speed, fuel_consumption, fuel_tank_capacity)
-        self.rocket_parameters = RocketParameters(initial_parameters, initial_rocket_mass, fuel)
+        self.rocket_parameters = RocketParameters(initial_rocket_mass, fuel)
 
     def set_predicative_orbit_log_size(self, new_size):
         self.constants.log_size = new_size
@@ -64,8 +63,8 @@ class PhysicsEngine:
     def set_parameters(self, new_parameters):
         self.rocket_parameters.parameters = new_parameters
 
-    def set_rocket_direction(self, new_direction):
-        self.rocket_parameters.direction = new_direction
+    def set_rocket_direction(self, angle):
+        self.rocket_parameters.direction = rotate_vector(self.rocket_parameters.direction, angle)
 
     def reduce_mass(self):
         if self.rocket_parameters.engine_is_on_flag and not self.rocket_parameters.is_empty():
@@ -126,12 +125,12 @@ class PhysicsEngine:
         """
         if self.rocket_parameters.engine_is_on_flag and not self.rocket_parameters.is_empty():
             if velocity_norm > 1e-8:
-                return self.constants.fuel_consumption * self.constants.gas_exhaust_speed / self.rocket_parameters.current_stage_mass \
-                       * self.rocket_parameters.direction
+                return self.constants.fuel_consumption * self.constants.gas_exhaust_speed / \
+                       self.rocket_parameters.current_stage_mass * self.rocket_parameters.direction
             else:
                 return np.array([
-                    self.constants.fuel_consumption * self.constants.gas_exhaust_speed / self.rocket_parameters.current_stage_mass,
-                    0])
+                    self.constants.fuel_consumption * self.constants.gas_exhaust_speed /
+                    self.rocket_parameters.current_stage_mass, 0])
         else:
             return np.zeros(2)
 
@@ -224,9 +223,10 @@ class PhysicsEngine:
         :return: array [[x, y, vx, vy], ...] consisting of predicative orbit points
         """
         log_size = self.constants.log_size
-        time_array = np.zeros(log_size)
+        time_array = np.ndarray(shape=(log_size,), dtype=float)
         time_array[0] = self.rocket_parameters.current_time
         predicative_orbit = np.ndarray(shape=(log_size, 4), dtype=float)
+        time_array[0] = 0
         predicative_orbit[0] = self.rocket_parameters.parameters
         count = 0
 
@@ -245,10 +245,17 @@ class PhysicsEngine:
         self.calc_predicative_orbit()
 
 
+def rotate_vector(vector, angle):
+    angle = np.deg2rad(angle)
+
+    rotation_matrix = np.array([[np.cos(angle), - np.sin(angle)], [np.sin(angle), np.cos(angle)]])
+
+    return np.dot(rotation_matrix, vector)
+
+
 if __name__ == "__main__":
-    initial_position = [10e6, 0, 0, 5000]
-    engine = PhysicsEngine(80000, 4000, 300, 50000, 50000, initial_position)
-    engine.switch_engine(True)
+    engine = PhysicsEngine(80000, 4000, 300, 50000, 50000)
+    engine.switch_engine(False)
 
     size = 500000
 
@@ -276,6 +283,7 @@ if __name__ == "__main__":
     ax.plot(predicative_orbit_log[::, 0], predicative_orbit_log[::, 1])
 
     plt.show()
+
 
 """
     first_stage = Stage([80000, 4000, 300, 50000, 50000])  # параметры первой ступени
