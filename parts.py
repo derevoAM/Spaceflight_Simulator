ceed76f44cb6161d3713fcba19b3e56579e39197
Rogodata <shumov.ai@phystech.edu>
2022-05-03 14:40:31 +0300

    added descriptions

--- parts.py
+++ parts.py
@@ -4,9 +4,19 @@ import pygame
 class Entity:
     """
     Просто класс, объект которого будет статично отрисован на ракете
+    Умеет отрисовывать себя на экране ракеты.
     """
 
     def __init__(self, surface, x=0, y=0, size=1, mass=0):
+        """
+        конструктор класса объекта детали
+        surface - объект класса pygame.surface
+        x, y - координаты левого верхнего угла изображения на экране.
+        size - размер детали(на будущее, маленький, большой или средний)
+        mass = масса детали
+        имеет поля текстуры (путь к файлу, где хранится изображение) и активности
+        (показывает программе, работает ли сейчас данный компонент)
+        """
         self.surface = surface
         self.x = x
         self.y = y
@@ -16,11 +26,25 @@ class Entity:
         self.active = 0
 
     def draw(self):
+        """
+        Рисует себя просто вклеиванием прямоугольника текстуры в нужное место холста ракеты
+        """
         self.surface.blit(self.texture, dest=[self.x, self.y])
 
 
 class Engine(Entity):
+    """
+    Класс ракетного двигателя
+    """
+
     def __init__(self, surface, power=0, consumption=0, x=0, y=0, mass=0):
+        """
+        Здесь уже имеются поля
+        consumption - потребление топлива
+        power - выходная мощность при максимальном потреблении горючего
+        output - процентная доля текущей мощности от выходной
+        тут в поле текстуры уже стоит определённый путь к файлу, а в поле типа - тип.
+        """
         Entity.__init__(self, surface, x=x, y=y, mass=mass)
         self.power = power
         self.consumption = consumption
@@ -31,7 +55,17 @@ class Engine(Entity):
 
 
 class FuelTank(Entity):
-    def __init__(self, surface, capacity = 0, x=0, y=0, mass=0):
+    """
+    Класс топливного бака
+    """
+
+    def __init__(self, surface, capacity=0, x=0, y=0, mass=0):
+        """
+        Здесь уже имеются поля
+        capacity - вместимость бака
+        fullness - процентная доля текущего количества топлива от полного
+        тут в поле текстуры уже стоит определённый путь к файлу, а в поле типа - тип.
+        """
         Entity.__init__(self, surface, x=x, y=y, mass=mass)
         self.capacity = capacity
         self.fullness = 100
@@ -40,19 +74,31 @@ class FuelTank(Entity):
 
 
 class Cabin(Entity):
+    """
+    класс кабины
+    """
+
     def __init__(self, surface, x=0, y=0, mass=0):
+        """
+        здесь прописан путь к текстуре и тип.
+        """
         Entity.__init__(self, surface, x=x, y=y, mass=mass)
         self.type = "cabin"
         self.texture = pygame.image.load('Textures/capsule/capsule_90x60.png')
 
 
 class Conector(Entity):
+    """
+    заготовка для будущей реализации класса соединителя
+    """
+
     def __init__(self, surface):
         Entity.__init__(self, surface)
         self.double = 1
         self.left = 0
 
 
+# Код для первичной проверки работы
 '''screen = pygame.display.set_mode((900, 900))
 engine = Engine(screen, 100 , 100, x=200, y=100)
 engine.draw()
